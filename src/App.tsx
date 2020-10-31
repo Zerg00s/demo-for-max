fc99b01bd10a23d2618c83418719e0db358abeb3
Denis Molodtsov <denis-ne@yandex.ru>
2020-10-30 22:44:41 -0400

    added security

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,8 @@ import { isThisTypeNode } from 'typescript';
 
 interface IState {
   currentUser: firebase.User,
-  messages: Message[],
-  notes: Note[]
+  messages: Message[]
+  // notes: Note[]
 }
 
 interface Message {
@@ -27,31 +27,100 @@ export default class App extends React.Component<IProps, IState> {
   public inputEl: HTMLInputElement;
   constructor(p: IProps) {
     super(p);
+
     this.inputEl = null;
     this.state = {
-      currentUser: firebase.auth().currentUser,
-      messages: [],
-      notes: []
-    }; // <- set up react state
+      currentUser: null,
+      messages: []
+    };
 
+    // <- set up react state
+    // if(this.state.currentUser){
+    //   this.addUser();
+    // }
+
+
+    /* Create reference to messages in Firebase Database */
 
 
   }
+
+
 
   componentWillMount() {
-    /* Create reference to messages in Firebase Database */
-    let messagesRef = fire.database().ref('notes').orderByKey().limitToLast(100);
-    messagesRef.on('child_added', snapshot => {
-      /* Update React state when message is added at Firebase Database */
-      let note = { text: snapshot.val(), id: snapshot.key };
-      this.setState({ notes: [note].concat(this.state.notes) });
+
+    const waitForCurrentUser = setInterval(() => {
+      if (firebase.auth().currentUser !== null) {
+        clearInterval(waitForCurrentUser);
+        this.setState({
+          currentUser: firebase.auth().currentUser
+        });
+
+        this.addUser();
+
+      } else {
+        console.log('Wait for it');
+      }
+    }, 100);
+
+
+    let messagesRef = fire.database().ref('messages');
+    if (messagesRef) {
+      messagesRef.on('child_added', snapshot => {
+        /* Update React state when message is added at Firebase Database */
+        let note = { text: snapshot.val(), id: snapshot.key };
+        this.setState({ messages: [note].concat(this.state.messages) });
+      })
+    }
+
+  }
+
+
+  render() {
+    return (
+      <>
+        {!firebase.auth().currentUser &&
+          <this.Authentication />
+        }
+
+        {firebase.auth().currentUser &&
+          <>
+            <this.SignOutButton />
+            <h2>{firebase.auth().currentUser.email}</h2>
+            <h2>{firebase.auth().currentUser.displayName}</h2>
+            <img src={firebase.auth().currentUser.photoURL}></img>
+          </>
+        }
+
+        <div>
+          <button onClick={() => this.addMessage()}>Add message!</button>
+        </div>
+
+        {this.state.messages && <ul>
+          {this.state.messages.map(message => <li key={message.id}>{message.text}</li>)}
+        </ul>
+        }
+
+      </>
+    );
+  }
+
+  addMessage = () => {
+    fire.database().ref('messages').push("test").catch(reason => {
+      console.log(reason);
+      // database rule:
+      //".write": "auth.token.email === 'molodtsovdenis@gmail.com'"
     })
   }
-  addMessage = () => {
-    // e.preventDefault(); // <- prevent form submit from reloading the page
-    /* Send the message to Firebase */
-    fire.database().ref('notes').push(this.inputEl.value);
-    this.inputEl.value = ''; // <- clear the input
+
+
+  addUser = () => {
+    return;
+    fire.database().ref('users').push({
+      uid: this.state.currentUser.uid,
+      name: this.state.currentUser.displayName,
+      email: this.state.currentUser.email
+    });
   }
 
   Authentication = () => {
@@ -72,6 +141,7 @@ export default class App extends React.Component<IProps, IState> {
 
   signOut = async () => {
     await firebase.auth().signOut();
+
     this.setState({
       currentUser: null
     });
@@ -85,23 +155,5 @@ export default class App extends React.Component<IProps, IState> {
     </>)
   }
 
-  render() {
-    return (
-      <>
-        {!firebase.auth().currentUser &&
-          <this.Authentication />
-        }
 
-        {firebase.auth().currentUser &&
-          <>
-            <this.SignOutButton />
-            <h2>{firebase.auth().currentUser.email}</h2>
-            <h2>{firebase.auth().currentUser.displayName}</h2>
-            <img src={firebase.auth().currentUser.photoURL}></img>
-          </>
-        }
-
-      </>
-    );
-  }
 }
